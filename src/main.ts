f273e74a8920245dda6f96f93f9355d6c984be3f
Kevin You <kevin.you@outlook.com>
2023-01-03 23:59:47 -0600

    Add success and fail logs, add a return value, and clean up how main is called

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,26 @@ import { publishStatus } from './mastodonApiClient';
 
 // TODO: Use a library like `envalid` for environment variable type safety, instead of casting to string
 
-const main = async () => {
+export const main = async () => {
     const incidents = await getActiveIncidents(process.env.DALLAS_ALERTS_OPENDATA_TOKEN as string);
+    let numPublished = 0;
     for (let incident of incidents) {
         const message = formatIncidentMessage(incident);
-        await publishStatus(process.env.DALLAS_ALERTS_MASTODON_TOKEN as string, message, incident.incident_number);
+        try {
+            await publishStatus(process.env.DALLAS_ALERTS_MASTODON_TOKEN as string, message, incident.incident_number);
+            console.log(`Successfully published incident ${incident.incident_number}`);
+            numPublished++;
+
+        } catch (e) {
+            console.error(`Failed to publish incident ${incident.incident_number}`);
+        }
+    }
+    return {
+        totalIncidents: incidents.length,
+        numPublished
     }
 };
 
-main();
-
+if (require.main === module) {
+    main();
+}
