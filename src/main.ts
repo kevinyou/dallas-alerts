a0c3343f95987dc0c62fb6b7b08b40b0dfc6f716
Kevin You <kevin.you@outlook.com>
2023-01-04 00:47:49 -0600

    Parallelize call to Mastodon

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { formatIncidentMessage, getActiveIncidents } from './dallasPDData';
 import { publishStatus } from './mastodonApiClient';
+import bluebird from 'bluebird';
 
 // TODO: Use a library like `envalid` for environment variable type safety, instead of casting to string
 
 export const main = async () => {
     const incidents = await getActiveIncidents(process.env.DALLAS_ALERTS_OPENDATA_TOKEN as string);
     let numPublished = 0;
-    for (let incident of incidents) {
+
+    await bluebird.map(incidents, async (incident) => {
         const message = formatIncidentMessage(incident);
         try {
             await publishStatus(process.env.DALLAS_ALERTS_MASTODON_TOKEN as string, message, incident.incident_number);
@@ -16,7 +18,8 @@ export const main = async () => {
         } catch (e) {
             console.error(`Failed to publish incident ${incident.incident_number}`);
         }
-    }
+    }, { concurrency: 5 })
+
     return {
         totalIncidents: incidents.length,
         numPublished
