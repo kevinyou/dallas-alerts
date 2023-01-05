3dd2bf18781abe5bdd79f82d61a17af9910ed7c4
Kevin You <kevin.you@outlook.com>
2023-01-05 00:17:57 -0600

    Add envalid

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { formatIncidentMessage, getActiveIncidents } from './dallasPDData';
 import { publishStatus } from './mastodonApiClient';
+import { cleanEnv, str } from 'envalid';
 
 // TODO: Use a library like `envalid` for environment variable type safety, instead of casting to string
 
 export const main = async () => {
-    const incidents = await getActiveIncidents(process.env.DALLAS_ALERTS_OPENDATA_TOKEN as string);
+    const env = cleanEnv(process.env, {
+        DALLAS_ALERTS_OPENDATA_TOKEN: str(),
+        DALLAS_ALERTS_MASTODON_TOKEN: str(),
+    });
+
+    const incidents = await getActiveIncidents(env.DALLAS_ALERTS_OPENDATA_TOKEN);
     let numPublished = 0;
     for (let incident of incidents) {
         const message = formatIncidentMessage(incident);
         try {
-            await publishStatus(process.env.DALLAS_ALERTS_MASTODON_TOKEN as string, message, incident.incident_number);
+            await publishStatus(env.DALLAS_ALERTS_MASTODON_TOKEN, message, incident.incident_number);
             console.log(`Successfully published incident ${incident.incident_number}`);
             numPublished++;
 
